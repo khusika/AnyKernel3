33fb85476af2623b3c37d3442f355e0cd6dce965
khusika <khusikadhamar@gmail.com>
2018-12-08 22:37:07 +0700

    AK2: Update magisk backup dir
    As of https://github.com/topjohnwu/Magisk/commit/83ae66daea690cde0c135d35d639639e8723a2a1
    
    Signed-off-by: khusika <khusikadhamar@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -45,7 +45,7 @@ decompressed_image=/tmp/anykernel/kernel/Image
 compressed_image=$decompressed_image.gz
 if [ -f $compressed_image ]; then
   # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
-  if [ -d $ramdisk/.backup ]; then
+  if [ -d $ramdisk/.backup -o -d $ramdisk/.magisk ]; then
     ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
     $bin/magiskboot --decompress $compressed_image $decompressed_image;
     $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
